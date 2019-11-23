fb1f1aadc3c1eea0dbfae020f6d551a4ea40d5cc
Ryan Jung <ryan_jung@mba.berkeley.edu>
2019-11-23 14:13:51 -0800

    React Hooks web app

--- webapp-redux/src/views/reducers/index.ts
+++
@@ -1,4 +0,0 @@
-import { combineReducers } from 'redux';
-import nameReducer from './nameReducer';
-
-export default combineReducers ({ nameReducer });
--- webapp-redux/src/views/types/types.d.ts
+++
@@ -1,6 +0,0 @@
-// wrap all of our types here
-declare module "BaseballTypes" {
-  import { StateType, ActionType } from "typesafe-actions";
-  export type ReducerState = StateType<typeof import("../store/configureStore").default>;
-  export type RootAction = ActionType<typeof import("../actions/nameSearch")>;
-}
